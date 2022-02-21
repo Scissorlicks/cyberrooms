3b7a6ab8894422106a91f4c65944e09b71f3cde5
Scissorlicks <96486398+Scissorlicks@users.noreply.github.com>
2022-02-20 22:22:19 -0700

    TOS

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -22,12 +22,8 @@ export default function Footer() {
 					Developed by Illusive Media
 				</a>
 
-				<a
-					className='mt-4 text-sm text-white transition-all duration-300 opacity-75 hover:text-purple-600 hover:opacity-100'
-					href='TOS.html'
-					target='_blank'
-					rel='noreferrer'
-				>
+				<a className='mt-4 text-sm text-white transition-all duration-300 opacity-75 hover:text-purple-600 hover:opacity-100' href='TOS.html'>
+				
 					Terms of Service
 					</a>
 
