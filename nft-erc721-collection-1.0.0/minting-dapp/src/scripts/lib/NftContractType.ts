e1e7b935dd3c4b143cd2f5e808d1d0c3cffd5ee3
Scissorlicks <96486398+Scissorlicks@users.noreply.github.com>
2022-02-21 20:10:06 -0700

    Dapp

---
+++ nft-erc721-collection-1.0.0/minting-dapp/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("CyberRooms") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { CyberRooms as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
