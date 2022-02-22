47b8a60fcc7f8d4233f875a6c7a6167a87b205bd
brady smith <bradysmith1019@gmail.com>
2022-02-21 23:48:59 -0700

    Remove debugging prints

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Button({ icon, text }) {
-	console.log("Here");
 	return (
 			<a href="publicdapp\index.html">
 				<button className='py-2 px-4 bg-purple-600 hover:bg-purple-500 transition-all duration-300 ease-in-out rounded-sm leading-none animate-glow uppercase tracking-wider'>
