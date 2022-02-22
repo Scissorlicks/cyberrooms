14f6fca42b29e99c1f4a27853c751f828228cce7
brady smith <bradysmith1019@gmail.com>
2022-02-21 23:47:38 -0700

    Link button to correct mint page

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 export default function Button({ icon, text }) {
+	console.log("Here");
 	return (
-			<a href="minting-dapp\public\index.html">
+			<a href="publicdapp\index.html">
 				<button className='py-2 px-4 bg-purple-600 hover:bg-purple-500 transition-all duration-300 ease-in-out rounded-sm leading-none animate-glow uppercase tracking-wider'>
 					{icon === undefined ? "" : { icon }} {text}
 				</button>
